d7ac20f3abd9e0e6e67cf8e76b970f2e14ac7fdf
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2022-05-20 01:08:15 -0400

    User Service Implemented

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, Response
+from flask_pymongo import PyMongo
+from services.user_service import *
+from utils.constants import *
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = DB_URI
+mongo = PyMongo(app)
+
+
+@app.route(USERS_PATH, methods=["POST"])
+def create_users():
+    response = save_user(mongo, request)
+    return response
+
+@app.route(USERS_PATH, methods=['GET'])
+def get_users():
+    response = get_all_users(mongo)
+    return response
+
+@app.route(USERS_PATH + ID, methods=['GET'])
+def get_user(id):
+    response = get_user_by_id(mongo, id)
+    return response
+
+@app.route(USERS_PATH + ID, methods=['DELETE'])
+def delete_user(id):
+    response = delete_user_by_id(mongo, id)
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/utils/constants.py
@@ -0,0 +1,12 @@
+# Database
+DB_URI = "mongodb://localhost/ProjectDB"
+
+# Endpoints
+API = "/api"
+V1 = "/v1"
+ID = "/<id>"
+USERS_PATH = API + V1 + "/users"
+
+# Codes
+ERROR_CODE = 404
+CREATED_CODE = 201
