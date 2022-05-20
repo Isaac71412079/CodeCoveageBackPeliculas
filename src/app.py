09acb410773b83737ea9134e46744f89f0d7eae7
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2022-05-20 01:09:13 -0400

    Code formated

--- src/app.py
+++ src/app.py
@@ -14,20 +14,24 @@ def create_users():
     response = save_user(mongo, request)
     return response
 
-@app.route(USERS_PATH, methods=['GET'])
+
+@app.route(USERS_PATH, methods=["GET"])
 def get_users():
     response = get_all_users(mongo)
     return response
 
-@app.route(USERS_PATH + ID, methods=['GET'])
+
+@app.route(USERS_PATH + ID, methods=["GET"])
 def get_user(id):
     response = get_user_by_id(mongo, id)
     return response
 
-@app.route(USERS_PATH + ID, methods=['DELETE'])
+
+@app.route(USERS_PATH + ID, methods=["DELETE"])
 def delete_user(id):
     response = delete_user_by_id(mongo, id)
     return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -6,65 +6,68 @@ import re
 
 # Post User
 def save_user(mongo, request):
-    username = get_field(request,"username")
-    email = get_field(request,"email")
-    password = get_field(request,"password")
-    isAdmin = get_field(request,"isAdmin")
-    
+    username = get_field(request, "username")
+    email = get_field(request, "email")
+    password = get_field(request, "password")
+    isAdmin = get_field(request, "isAdmin")
+
     if username and email and password:
         body = {
-            "username": username, 
-            "password": password, 
+            "username": username,
+            "password": password,
             "email": email,
-            "isAdmin": isAdmin
+            "isAdmin": isAdmin,
         }
         _id = mongo.db.users.insert_one({**body})
-        response = jsonify({
-            "_id": str(_id.inserted_id),
-            **body
-        })
+        response = jsonify({"_id": str(_id.inserted_id), **body})
         response.status_code = CREATED_CODE
         return response
     else:
         return error(request)
- 
-# Get All Users  
+
+
+# Get All Users
 def get_all_users(mongo):
     users = mongo.db.users.find()
     response = json_util.dumps(users)
     response = remove_oid(response)
     return Response(response, mimetype="application/json")
 
-# Get User by ID 
+
+# Get User by ID
 def get_user_by_id(mongo, id):
-    user = mongo.db.users.find_one({'_id': ObjectId(id)})
+    user = mongo.db.users.find_one({"_id": ObjectId(id)})
     response = json_util.dumps(user)
     response = remove_oid(response)
     return Response(response, mimetype="application/json")
 
+
 # Delete User by ID
 def delete_user_by_id(mongo, id):
-    userDeleted = mongo.db.users.delete_one({'_id': ObjectId(id)})
-    response = jsonify({'message': 'User ' + id + ' Deleted Successfully'})
+    userDeleted = mongo.db.users.delete_one({"_id": ObjectId(id)})
+    response = jsonify({"message": "User " + id + " Deleted Successfully"})
     response.status_code = 200
     return response
+
 
 # Helpers
 def error(request):
     message = {
-        'message': 'Oops! Something went wrong - ' + request.url,
-        'status': ERROR_CODE
+        "message": "Oops! Something went wrong - " + request.url,
+        "status": ERROR_CODE,
     }
     response = jsonify(message)
     response.status_code = ERROR_CODE
     return response
 
+
 def get_field(request, field):
-    return request.json.get(field, None);
+    return request.json.get(field, None)
+
 
 def remove_oid(string):
     while True:
-        pattern = re.compile('{\s*"\$oid":\s*(\"[a-z0-9]{1,}\")\s*}')
+        pattern = re.compile('{\s*"\$oid":\s*("[a-z0-9]{1,}")\s*}')
         match = re.search(pattern, string)
         if match:
             string = string.replace(match.group(0), match.group(1))
